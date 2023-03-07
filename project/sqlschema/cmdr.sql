d186a1201d2375e8e1982049a53475791fbbfa93
Mark A. Miller <MAM@lbl.gov>
2023-03-06 17:13:50 -0800

    with rendering

--- project/sqlschema/cmdr.sql
+++ project/sqlschema/cmdr.sql
@@ -1,41 +1,56 @@
 
 
-CREATE TABLE "Database" (
+CREATE TABLE "Container" (
+	materials TEXT, 
+	participations TEXT, 
+	processes TEXT, 
+	investigations TEXT, 
 	subjects TEXT, 
-	PRIMARY KEY (subjects)
+	PRIMARY KEY (materials, participations, processes, investigations, subjects)
 );
 
-CREATE TABLE "Diagnosis" (
-	is_about TEXT, 
-	source TEXT, 
-	PRIMARY KEY (is_about, source)
+CREATE TABLE "Investigation" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	part_of TEXT, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(part_of) REFERENCES "Investigation" (id)
 );
 
-CREATE TABLE "Document" (
-	is_about TEXT, 
-	PRIMARY KEY (is_about)
-);
-
-CREATE TABLE "Process" (
+CREATE TABLE "MaterialProcessing" (
+	id TEXT NOT NULL, 
 	has_input TEXT, 
 	has_output TEXT, 
-	PRIMARY KEY (has_input, has_output)
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE "Sampling" (
+CREATE TABLE "SpecimenCollectionProcess" (
+	id TEXT NOT NULL, 
 	has_input TEXT, 
 	has_output TEXT, 
-	PRIMARY KEY (has_input, has_output)
-);
-
-CREATE TABLE "Specimen" (
-	has_source TEXT, 
-	has_parent_specimen TEXT, 
-	PRIMARY KEY (has_source, has_parent_specimen)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE "Subject" (
-	taxon TEXT, 
-	birth_date TEXT, 
-	PRIMARY KEY (taxon, birth_date)
+	id TEXT NOT NULL, 
+	name TEXT, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE "MaterialEntity" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	used_in TEXT, 
+	source TEXT, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(source) REFERENCES "Subject" (id)
+);
+
+CREATE TABLE "Participation" (
+	includes TEXT, 
+	id TEXT NOT NULL, 
+	name TEXT, 
+	involved_in TEXT, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(includes) REFERENCES "Subject" (id)
 );
