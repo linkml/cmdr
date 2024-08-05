6965acf5d3a43356421ad464cc5a530c18f97ce1
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-08-05 13:18:16 -0700

    cruft update

--- src/cmdr/datamodel/__init__.py
+++ src/cmdr/datamodel/__init__.py
@@ -1,1 +1,7 @@
+from pathlib import Path
 from .cmdr import *
+
+THIS_PATH = Path(__file__).parent
+
+SCHEMA_DIRECTORY = THIS_PATH.parent / "schema"
+MAIN_SCHEMA_PATH = SCHEMA_DIRECTORY / "cmdr.yaml"
